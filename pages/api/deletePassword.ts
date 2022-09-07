062f3033a25201719922d14fb69cba67c161a364
Lars van der Niet <lvdnbusiness@icloud.com>
2022-09-07 13:42:16 +0200

    Added some homepage text and removed imports

--- pages/api/deletePassword.ts
+++ pages/api/deletePassword.ts
@@ -1,28 +1,27 @@
 import { database } from "@lib/firebaseConfig";
-import { collection, deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 
 import type { NextApiRequest, NextApiResponse } from "next";
-const dbInstance = collection(database, "passwords");
 
 type Data = {
-	uniqueID?: string;
-	error?: string;
+    uniqueID?: string;
+    error?: string;
 };
 
 export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-	// Get reference to document
-	const uniqueID = req.body.uniqueID;
-	const docRef = doc(database, "passwords", uniqueID);
+    // Get reference to document
+    const uniqueID = req.body.uniqueID;
+    const docRef = doc(database, "passwords", uniqueID);
 
-	// Delete document
-	deleteDoc(docRef)
-		.then(() => {
-			res.status(200).json({ uniqueID });
-		})
-		.catch((error) => {
-			res.status(500).json({ error });
-		});
+    // Delete document
+    deleteDoc(docRef)
+        .then(() => {
+            res.status(200).json({ uniqueID });
+        })
+        .catch((error) => {
+            res.status(500).json({ error });
+        });
 }
