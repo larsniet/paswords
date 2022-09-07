0016b73590db939c4eddf44aa4d2818aa9c8c28c
Lars van der Niet <larsvanderniet@gmail.com>
2022-09-07 10:50:19 +0200

    test dangerous skipping types

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
 	images: {
 		domains: ["picsum.photos"],
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
 };
 
 module.exports = nextConfig;
