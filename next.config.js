ba7d7944356c905d7e127276841c5699fc286b83
Lars van der Niet <lvdnbusiness@icloud.com>
2022-09-07 16:52:31 +0200

    Fixed lighthouse stuff

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require("next-pwa")({
+    dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -10,4 +14,4 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
