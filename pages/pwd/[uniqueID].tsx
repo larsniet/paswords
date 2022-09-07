cc2675c6fff8a9057d9e5190cc919cbc8393ecbd
Lars van der Niet <lvdnbusiness@icloud.com>
2022-09-07 14:10:01 +0200

    More cleanup

--- pages/pwd/[uniqueID].tsx
+++ pages/pwd/[uniqueID].tsx
@@ -9,114 +9,118 @@ import { useEffect, useRef } from "react";
 
 import type { NextPage } from "next";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 type PasswordPageProps = {
-	uniqueID: string;
-	decryptedPass: string;
+    uniqueID: string;
+    decryptedPass: string;
 };
 
 const PasswordPage: NextPage<PasswordPageProps> = ({
-	uniqueID,
-	decryptedPass,
+    uniqueID,
+    decryptedPass,
 }) => {
-	// Delete password from database
-	fetch("/api/deletePassword", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			uniqueID,
-		}),
-	});
+    // Delete password from database
+    fetch(baseUrl + "/api/deletePassword", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            uniqueID,
+        }),
+    });
 
-	// This fixes an issue with NextJS images using inline-block
-	const imgRef = useRef(null);
-	useEffect(() => {
-		imgRef.current.firstChild.style.display = "block";
-	}, []);
+    // This fixes an issue with NextJS images using inline-block
+    const imgRef = useRef(null);
+    useEffect(() => {
+        imgRef.current.firstChild.style.display = "block";
+    }, []);
 
-	// If the password is not found, show loading
-	if (!decryptedPass) {
-		return <Loading />;
-	}
+    // If the password is not found, show loading
+    if (!decryptedPass) {
+        return <Loading />;
+    }
 
-	const copyToClipboard = (e) => {
-		// Write to clipboard and show notification
-		navigator.clipboard.writeText(decryptedPass);
-		e.target.innerHTML = "Copied to clipboard!";
-		e.target.classList.add("btn-success");
+    const copyToClipboard = (e) => {
+        // Write to clipboard and show notification
+        navigator.clipboard.writeText(decryptedPass);
+        e.target.innerHTML = "Copied to clipboard!";
+        e.target.classList.add("btn-success");
 
-		// After 2 seconds, change the text back
-		setTimeout(() => {
-			e.target.classList.remove("btn-success");
-			e.target.innerHTML = decryptedPass;
-		}, 2000);
-	};
+        // After 2 seconds, change the text back
+        setTimeout(() => {
+            e.target.classList.remove("btn-success");
+            e.target.innerHTML = decryptedPass;
+        }, 2000);
+    };
 
-	return (
-		<>
-			<Head>
-				<title>Test</title>
-				<meta
-					name="description"
-					content="Pasword allows users from all over the world to share passwords in a secure way. Using our one-time-only link feature no-one gets access to your password except the people you choose."
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<div className="hero min-h-screen bg-base-200">
-				<div className="hero-content flex-col">
-					<div className="card lg:card-side bg-base-100 shadow-xl">
-						<div className="card-body max-w-md">
-							<h2 className="card-title">You made it!</h2>
-							<p>
-								Someone needed you to have this password, so here you go. Make
-								sure to keep it safe!
-								<br />
-								<br />
-								To copy the password, simply click the password in the button
-								below.
-								<br />
-								<br />
-								<button
-									onClick={(e) => copyToClipboard(e)}
-									className="btn w-full break-all"
-								>
-									{decryptedPass}
-								</button>
-							</p>
-							<div className="card-actions">
-								Would you like to send a password?
-								<Link href="/">
-									<div className="btn btn-primary btn-xs">Click here</div>
-								</Link>
-							</div>
-						</div>
-						<figure ref={imgRef} className="hidden lg:block">
-							<Image
-								src="https://picsum.photos/400/550"
-								width={400}
-								height={500}
-								alt="Random image from picsum photos"
-							/>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <title>Test</title>
+                <meta
+                    name="description"
+                    content="Pasword allows users from all over the world to share passwords in a secure way. Using our one-time-only link feature no-one gets access to your password except the people you choose."
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="card lg:card-side bg-base-100 shadow-xl">
+                        <div className="card-body max-w-md">
+                            <h2 className="card-title">You made it!</h2>
+                            <p>
+                                Someone needed you to have this password, so
+                                here you go. Make sure to keep it safe!
+                                <br />
+                                <br />
+                                To copy the password, simply click the password
+                                in the button below.
+                                <br />
+                                <br />
+                                <button
+                                    onClick={(e) => copyToClipboard(e)}
+                                    className="btn w-full break-all"
+                                >
+                                    {decryptedPass}
+                                </button>
+                            </p>
+                            <div className="card-actions">
+                                Would you like to send a password?
+                                <Link href="/">
+                                    <div className="btn btn-primary btn-xs">
+                                        Click here
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                        <figure ref={imgRef} className="hidden lg:block">
+                            <Image
+                                src="https://picsum.photos/400/550"
+                                width={400}
+                                height={500}
+                                alt="Random image from picsum photos"
+                            />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export const getServerSideProps = async (ctx) => {
-	const uniqueID = ctx.query.uniqueID;
-	const docRef = doc(database, "passwords", uniqueID);
-	const docSnap = await getDoc(docRef);
-	const data = docSnap.data();
+    const uniqueID = ctx.query.uniqueID;
+    const docRef = doc(database, "passwords", uniqueID);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
 
-	if (!data) return { notFound: true };
-	const cipher = data.encryptedPass as CipherType;
-	const decryptedPass = decryptPass(cipher);
+    if (!data) return { notFound: true };
+    const cipher = data.encryptedPass as CipherType;
+    const decryptedPass = decryptPass(cipher);
 
-	return { props: { decryptedPass, uniqueID } };
+    return { props: { decryptedPass, uniqueID } };
 };
 
 export default PasswordPage;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,55 +1,60 @@
 import { secondsToDhms } from "@lib/helpers";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 type ModalProps = {
-	uniqueID: string;
-	validInSec: number;
+    uniqueID: string;
+    validInSec: number;
 };
 
 const Modal: React.ElementType<ModalProps> = ({ uniqueID, validInSec }) => {
-	const baseURL = process.env.BASEURL || "http://localhost:3000";
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-	useEffect(() => {
-		// Get the modal
-		const modal = document.getElementById(
-			"success-modal"
-		) as HTMLInputElement | null;
-		if (uniqueID && validInSec && modal) {
-			modal.checked = true;
-		} else {
-			modal.checked = false;
-		}
-	}, [uniqueID, validInSec]);
+    useEffect(() => {
+        // Get the modal
+        const modal = document.getElementById(
+            "success-modal"
+        ) as HTMLInputElement | null;
+        if (uniqueID && validInSec && modal) {
+            modal.checked = true;
+        } else {
+            modal.checked = false;
+        }
+    }, [uniqueID, validInSec]);
 
-	return (
-		<>
-			<input type="checkbox" id="success-modal" className="modal-toggle" />
-			<div className="modal modal-bottom sm:modal-middle">
-				<div className="modal-box">
-					<h3 className="font-bold text-lg">
-						Your password has been safely encrypted!
-					</h3>
-					<p className="py-4">
-						You can now share the link below with anyone you would like. Keep in
-						mind, the link will only be valid for {secondsToDhms(validInSec)}{" "}
-						and once opened it will be erased forever.
-					</p>
-					<p className="pb-4">
-						<a
-							href={`${baseURL + "/pwd/" + uniqueID}`}
-							className="text-blue-500"
-						>
-							{`${baseURL + "/pwd/" + uniqueID}`}
-						</a>
-					</p>
-					<div className="flex justify-end space-x-3">
-						<button className="btn btn-ghost">Cancel</button>
-						<button className="btn btn-primary">Copy</button>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <input
+                type="checkbox"
+                id="success-modal"
+                className="modal-toggle"
+            />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">
+                        Your password has been safely encrypted!
+                    </h3>
+                    <p className="py-4">
+                        You can now share the link below with anyone you would
+                        like. Keep in mind, the link will only be valid for{" "}
+                        {secondsToDhms(validInSec)} and once opened it will be
+                        erased forever.
+                    </p>
+                    <p className="pb-4">
+                        <a
+                            href={`${baseURL + "/pwd/" + uniqueID}`}
+                            className="text-blue-500"
+                        >
+                            {`${baseURL + "/pwd/" + uniqueID}`}
+                        </a>
+                    </p>
+                    <div className="flex justify-end space-x-3">
+                        <button className="btn btn-ghost">Cancel</button>
+                        <button className="btn btn-primary">Copy</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Modal;
