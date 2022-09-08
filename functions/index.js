47ec28aee6e1785b089b91fe6c44023466ce863c
Lars van der Niet <lvdnbusiness@icloud.com>
2022-09-08 19:18:43 +0200

    Updated functions logic

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,18 @@
 const functions = require("firebase-functions");
 
 exports.createPassword = functions.firestore
-	.document("passwords/{passwordId}")
-	.onCreate(async (snapshot, context) => {
-		const obj = snapshot.data();
-		const validInSec = obj.validInSec;
+    .document("passwords/{passwordId}")
+    .onCreate(async (snapshot, context) => {
+        const obj = snapshot.data();
+        const validInSec = obj.validInSec;
 
-		setTimeout(() => {
-			snapshot.ref.delete();
-		}, validInSec * 1000);
-	});
+        return deletePassword(snapshot.ref, validInSec);
+    });
+
+const deletePassword = (ref, validInSec) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            ref.delete().then(resolve, reject);
+        }, validInSec * 1000);
+    });
+};
