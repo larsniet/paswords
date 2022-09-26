156bae8d20480b63a83f452f82c3e42129d2ac4c
larsniet <lvdnbusiness@icloud.com>
2022-09-26 13:10:24 +0200

    Added cleaner and more reliable password removal logic

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,35 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.createPassword = functions.firestore
-    .document("passwords/{passwordId}")
-    .onCreate(async (snapshot, context) => {
-        const obj = snapshot.data();
-        const validInSec = obj.validInSec;
+admin.initializeApp(functions.config().firebase);
+admin.firestore().settings({ timestampsInSnapshots: true });
 
-        return deletePassword(snapshot.ref, validInSec);
+exports.checkPasswordsEveryMinute = functions.pubsub
+    .schedule("* * * * *")
+    .onRun(async () => {
+        // Get all passwords
+        const passwords = await admin.firestore().collection("passwords").get();
+
+        // Delete all passwords where now > createdAtInSec + validUntilInSec
+        passwords.forEach((password) => {
+            const obj = password.data();
+            const validInSec = obj.validInSec;
+            const createdAtInSec = password.createTime.seconds;
+
+            // Add validInSec to createdAtInSec
+            const validUntilInSec = createdAtInSec + validInSec;
+            const currentTimeInSec = Math.floor(Date.now() / 1000);
+
+            console.log(
+                `Password ${password.id} should be valid until: ${validUntilInSec}. Currently it is: ${currentTimeInSec}`
+            );
+
+            // Check if validUntilInSec is more than current time
+            if (currentTimeInSec > validUntilInSec) {
+                console.log("Deleting password: " + password.id);
+                password.ref.delete();
+            }
+        });
+
+        return null;
     });
-
-const deletePassword = (ref, validInSec) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            ref.delete().then(resolve, reject);
-        }, validInSec * 1000);
-    });
-};
