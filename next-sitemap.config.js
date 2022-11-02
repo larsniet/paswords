adb31ed117a489d14066d037c1a48c2d1474079e
larsniet <lvdnbusiness@icloud.com>
2022-11-02 11:59:11 +0100

    Added disallow for /pwd

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,16 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://paswords.link",
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+            {
+                userAgent: "*",
+                disallow: ["/pwd"],
+            },
+        ],
+    },
 };
